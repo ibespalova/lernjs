8e5965b2c2590451863db625082e17cc1214baf1
Irina Bespaova <ibespalova.a94@gmail.com>
2020-01-25 16:39:11 +0200

    Второй урок

---
+++ script.js
@@ -0,0 +1,21 @@
+let money = 1;
+console.log(typeof money);
+
+let income = 'freelance';
+
+console.log(typeof income);
+
+let addExpenses = 'Internet, Taxi, Clothes';
+console.log(addExpenses.length);
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+
+let deposit = true;
+console.log(typeof deposit);
+
+let mission = 1100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000; 
+let period = 12;
+console.log('Период равен ' +period+ ' месяцев' + ' Цель заработать' +mission+ 'кусочков');
+
+let budgetDay = money/30;
+console.log(budgetDay);
