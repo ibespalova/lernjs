7777ea6a5f69ab9dc3314564fac6ce7fab70389f
Irina Bespalova <ibespalova.a94@gmail.com>
2020-01-27 21:01:34 +0200

    сделано почти все

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-let money = 1;
+let money = prompt('Ваш месячный доход');
 console.log(typeof money);
+console.log(money);
 
 let income = 'freelance';
+console.log(income);
 
-console.log(typeof income);
-
-let addExpenses = 'Internet, Taxi, Clothes';
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую')
+console.log(addExpenses);
 console.log(addExpenses.length);
 console.log(addExpenses.toLowerCase());
 console.log(addExpenses.split(', '));
 
-let deposit = true;
+let deposit = confirm('Есть ли у вас депозит в банке');
 console.log(typeof deposit);
+
+let expenses1 = prompt('Введите обязательную статью расходов?');
+let expenses2 = prompt('Введите обязательную статью расходов?');
+let amount1 = prompt('Во сколько это обойдется?');
+let amount2 = prompt('Во сколько это обойдется?');
+
+let budgetMonth = money - amount1 - amount2;
+console.log(budgetMonth);
 
 let mission = 1100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000; 
 let period = 12;
-console.log('Период равен ' +period+ ' месяцев' + ' Цель заработать' +mission+ 'кусочков');
+console.log('Период равен ' +period+ ' месяцев.' + ' Цель заработать ' +mission+ ' денег');
 
 let budgetDay = money/30;
 console.log(budgetDay);
+
+
+
